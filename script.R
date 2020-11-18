f2ab71c56f6d393fdddc33b2c855d9be1b7a6865
Daniel Medina <danielmedinaneira555@gmail.com>
2020-11-18 16:38:14 -0500

    change script

--- script.R
+++ script.R
@@ -2,7 +2,7 @@ library(fs) # manipulate files
 library(readxl) # read Excel files
 library(stringr) # manipulate strings
 library(dplyr) # data management
-setwd(path("C:", "Users", "NAPO", "Desktop", "languages_anki"))
+setwd(path("C:", "Users", "NAPO", "Desktop", "languages_anki")) # set working directory
 
 # read data set of the 625 words
 dat <- read_xlsx(path("625_words", "data_set_flashcards.xlsx"), col_names = FALSE)
@@ -13,7 +13,10 @@ word_list <- readLines(path("625_words", "original_list.txt"))
 
 clean_words <- str_extract(word_list, regex("\\w+"))
 dat$...2
-
+# check the conflicting words between the data set and the original list
 c(clean_words[!clean_words %in% dat$...2], dat$...2[!dat$...2 %in% clean_words])
-
+# check the redundant words
 word_list %>% str_subset("\\w+/\\w+")
+
+# obtain the names of the image with numbers (this means different images for same words)
+img_names %>% str_subset("\\w+\\d")
