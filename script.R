be8e518a3f10e67017c593bfb918f624390fb5eb
Daniel Medina <danielmedinaneira555@gmail.com>
2020-11-18 16:32:52 -0500

    remove the redundant words from the data set

--- script.R
+++ script.R
@@ -2,6 +2,7 @@ library(fs) # manipulate files
 library(readxl) # read Excel files
 library(stringr) # manipulate strings
 library(dplyr) # data management
+setwd(path("C:", "Users", "NAPO", "Desktop", "languages_anki"))
 
 # read data set of the 625 words
 dat <- read_xlsx(path("625_words", "data_set_flashcards.xlsx"), col_names = FALSE)
@@ -12,3 +13,7 @@ word_list <- readLines(path("625_words", "original_list.txt"))
 
 clean_words <- str_extract(word_list, regex("\\w+"))
 dat$...2
+
+c(clean_words[!clean_words %in% dat$...2], dat$...2[!dat$...2 %in% clean_words])
+
+word_list %>% str_subset("\\w+/\\w+")
