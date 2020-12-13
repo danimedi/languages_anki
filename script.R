8624db685a0e5672daebfa341a77852edf3405de
Daniel Medina <danielmedinaneira555@gmail.com>
2020-12-13 07:46:33 -0500

    delete excel data set

--- script.R
+++ script.R
@@ -1,24 +1,30 @@
 library(fs) # manipulate files
-library(readxl) # read Excel files
 library(stringr) # manipulate strings
 library(dplyr) # data management
+library(readr) # read files
 
 # read data set of the 625 words
-dat <- read_xlsx(path("625_words", "data_set_flashcards.xlsx"), col_names = FALSE)
+dat <- read_csv2(path("625_words", "data_set_flashcards.csv"), col_names = FALSE)
 # get names of images
 img_names <- list.files(path("625_words", "media"))
 # original list of words
 word_list <- readLines(path("625_words", "original_list.txt"))
 
 # images in media that are not in the data set
-img_data_set <- str_extract_all(dat$...1, "\\w+[.]jpg") %>% unlist() # image names from the data set
+img_data_set <- str_extract_all(dat$X1, "\\w+[.]jpg") %>% unlist() # image names from the data set
 img_names[!img_names %in% img_data_set]
 # images in the data set that are not in media
 img_data_set[!img_data_set %in% img_names]
 
 # obtain the repeated words (same word, different meanings), contrast this with the original list
-freqs <- table(dat$...2)
+freqs <- table(dat$X2)
 names(freqs)[freqs > 1]
+
+# change the names of the images in the data set
+actual_words <- str_remove_all(dat$X1, "(<img src=\"|\">)")
+new_words <- paste0("vocab-", actual_words)
+dat$X1 <- paste0("<img src=\"", new_words, "\">")
+# write_excel_csv2(dat, "625_words/data_set_flashcards.csv", col_names = FALSE)
 
 
 # check translations based on multiple languages --------------------------
