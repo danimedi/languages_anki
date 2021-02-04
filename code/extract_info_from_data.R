6e5fcffa983db604deb5d0b4955c579c5ff5d82c
napo-medina <diego.napoleon.dm@gmail.com>
2021-02-03 19:01:44 -0500

    different changes in code

--- code/extract_info_from_data.R
+++ code/extract_info_from_data.R
@@ -1,7 +1,8 @@
 library(here)
+library(readr)
 
 # read data set of the 625 words
-data_set <- read.csv2(here("data_625_words", "data_set_flashcards.csv"), col_names = FALSE)
+data_set <- read_csv2(here("data_625_words", "data_set_flashcards.csv"), col_names = FALSE)
 # get names of images
 img_names <- list.files(here("data_625_words", "media"))
 # original list of words
